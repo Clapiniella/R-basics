7a78de3085753791b3574ba9b9500feaca318112
Clapiniella <clapiniella@gmail.com>
2020-09-20 14:00:43 +0200

    updating repo

---
+++ day1/interfaz_RStudio.R
@@ -0,0 +1,10 @@
+# WEEK 11 - day 1
+print("hello")
+# para ejecutar selecciona el trozo de código y Ctrl+Enter
+
+num_alumnos <- 12
+
+example <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8), nrow = 4, ncol = 2)
+print(example)
+
+plot(example)
