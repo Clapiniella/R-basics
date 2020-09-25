7e71ba498ba278fc5d588c458c2d45b362d0abb6
Clapiniella <clapiniella@gmail.com>
2020-09-25 11:40:45 +0200

    done

--- day4/W11D4linearregression.R
+++ day4/W11D4linearregression.R
@@ -1,24 +1,29 @@
 # WEEK 11 - day 3 Caret
 
+# tarda mucho en instalarse
+# install.packages("caret",  dependencies = c("Depends", "Suggests"))
 library(caret)
 library(ggplot2)
+library(lattice)
 
 #####################
 # Linear Regression #
 #####################
 
 data(mtcars)
+head(mtcars)
 ?mtcars
 # mpg --> Miles/(US) gallon <-- variable dependiente (a predecir)
 # wt --> Weight (1000 lbs) <-- variable independiente
 
-set.seed(333)
+set.seed(42)
 
 # ploteamos ambas variables
 
-plot(x = mtcars$wt, y =mtcars$mpg)
+plot(x = mtcars$wt, y = mtcars$mpg)
 
 # creamos la regresión linear con R
+# mpg ~ .
 
 fit <- lm(mpg ~ wt, data = mtcars)
 
@@ -27,6 +32,7 @@ fit <- lm(mpg ~ wt, data = mtcars)
 abline(fit)
 
 # para conocer los datos de entrenamiento
+# https://feliperego.github.io/blog/2015/10/23/Interpreting-Model-Output-In-R
 summary(fit)
 
 # añadimos la ecuacion al plot
@@ -41,6 +47,7 @@ mtext(lmlab, 3, line=-2)
 # dividimos train/test
 # indicamos cual es la variable a predecir
 inTrain <- createDataPartition(y = mtcars$mpg, p = 0.6, list = FALSE)
+
 training <- mtcars[inTrain,]
 test <- mtcars[-inTrain,]
 
@@ -48,25 +55,31 @@ dim(training)
 dim(test)
 
 # ploteamos los datos de entrenamiento
-plot(x= mtcars$wt, y = mtcars$mpg)
+plot(x= training$wt, y = training$mpg)
 
 # entrenamos el modelo
 # el resultado sera similar a lo hecho anteriormente
+# virgulilla
 fit_train <- train(mpg ~ wt, data= training, method = 'lm', metric="RMSE")
 fit_train$finalModel
 
 # ploteamos el modelo generado
 pred_carstrain <- predict(fit_train, newdata = training)
+
+
 lines(x = training$wt, y = pred_carstrain)
 
 # calculamos el error RMSE
 cal_rmse <- RMSE(pred_carstrain, training$mpg)
 
-# predecimos con el conjunto de test
+
+# ploteamos los datos de test
 plot(x = test$wt, y=test$mpg)
 
-# ploteamos la predicción sobre el test
+# predecimos con el conjunto de test
+
 pred_carstest <- predict(fit_train, newdata=test)
+# ploteamos la predicción sobre el test
 lines(x=test$wt, y=pred_carstest )
 
 # evaluamos con test 
@@ -74,5 +87,4 @@ comparing <- data.frame(pred = pred_carstest, real = test$mpg)
 
 # calculamos error
 postResample(pred_carstest, test$mpg)
-
 
